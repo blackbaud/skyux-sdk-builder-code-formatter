d88e314e0c3f637b343ce1b89079cb6924cc6566
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-04-14 12:32:31 -0400

    Add build scripts

---
+++ scripts/prepare-package.js
@@ -0,0 +1,45 @@
+/* jshint node: true */
+
+'use strict';
+
+const fs = require('fs-extra');
+const glob = require('glob');
+const path = require('path');
+const rimraf = require('rimraf');
+
+const rootPath = path.join(__dirname, '..');
+const distPath = path.join(rootPath, 'dist');
+
+function copyFiles() {
+  fs.ensureDirSync(distPath);
+  const files = glob.sync(
+    path.join(
+      rootPath,
+      '{index.js,package.json,README.md,/lib/**/*.js,/config/**/*}'
+    ),
+    {
+      nodir: true,
+      ignore: ['**/*.spec.js'],
+      dot: true
+    }
+  );
+  files.forEach((file) => {
+    const copyDest = path.join(
+      distPath,
+      path.join(file).replace(process.cwd(), '')
+    );
+    fs.ensureFileSync(copyDest);
+    fs.copyFileSync(path.join(file), copyDest);
+  });
+}
+
+function createDist() {
+  return new Promise((resolve) => {
+    rimraf(distPath, () => {
+      copyFiles();
+      resolve();
+    });
+  });
+}
+
+createDist();
